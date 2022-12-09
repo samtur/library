65b3862fc8907eff48d62b57b268a0025c34751e
samtur <samturland23@gmail.com>
2022-12-09 22:53:01 +0000

    Fix toggle and delete functionality

--- script.js
+++ script.js
@@ -4,16 +4,8 @@ let bookAuthor = document.querySelector("#author");
 let bookStatus = document.querySelector("#status");
 const submit = document.querySelector("#submitBtn");
 const contentContainer = document.querySelector(".contentContainer");
-let readBtn = document.querySelectorAll(".contentThree");
-
-// GET THIS TO WORK!
-// readBtn = document.querySelectorAll(".contentThree");
-
-// readBtn.forEach((item) => {
-//   readBtn.addEventListener("click", function (e) {
-//     console.log(item);
-//   });
-// });
+let readBtns = document.querySelectorAll(".contentThree");
+let deleteBtns = document.querySelectorAll(".contentFour");
 
 // Array to store books
 let library = [];
@@ -32,6 +24,7 @@ submit.addEventListener("click", function (e) {
   bookNameValue = bookName.value;
   bookAuthorValue = bookAuthor.value;
   bookStatusValue = bookStatus.value;
+  bookStatusValue = bookStatusValue === "true";
   bookName.value = "";
   bookAuthor.value = "";
   bookStatus.value = "";
@@ -40,18 +33,53 @@ submit.addEventListener("click", function (e) {
 });
 
 // Sample data
-let harryPotter = new Book(
-  "Harry Potter and the Goblet of Fire",
-  "JK Rowling",
-  true
-);
-let lordOfTheRings = new Book(
-  "Lord of the Rings Trilogy",
-  "JRR Tolkein",
-  false
-);
-let kafkaOnTheShore = new Book("Kafka on the Shore", "Haruki Murakami", true);
-library.push(harryPotter, lordOfTheRings, kafkaOnTheShore);
+// let harryPotter = new Book(
+//   "Harry Potter and the Goblet of Fire",
+//   "JK Rowling",
+//   true
+// );
+// let lordOfTheRings = new Book(
+//   "Lord of the Rings Trilogy",
+//   "JRR Tolkein",
+//   false
+// );
+// let kafkaOnTheShore = new Book("Kafka on the Shore", "Haruki Murakami", true);
+// library.push(harryPotter, lordOfTheRings, kafkaOnTheShore);
+
+// Function to toggle read buttons
+function readListener(readBtns) {
+  readBtns.forEach((btn) => {
+    btn.addEventListener("click", function (e) {
+      e.preventDefault();
+      btn.textContent === "read"
+        ? (btn.textContent = "not read")
+        : (btn.textContent = "read");
+    });
+    // library[i].read === true
+    //   ? (library[i].read = false)
+    //   : (library[i].read = true);
+  });
+}
+// Function to delete books
+// function deleteListener(deleteBtns) {
+//   for (let i = 0; i < deleteBtns.length; i++) {
+//     deleteBtns[i].addEventListener("click", function () {
+//       console.log(library);
+//       library.splice(i, 1);
+//       console.log(library);
+//       const item = document.querySelector(`#contentRow${i}`);
+//       item.remove();
+//     });
+//   }
+// }
+
+// Function to update ids
+// function updateId(contentRows) {
+//   for (let i = 0; i < contentRows.length; i++) {
+//     console.log(contentRows[i]);
+//     contentRows[i].id = `contentRow${i}`;
+//   }
+// }
 
 // Function to add book to array.
 function addBookToLibrary() {
@@ -66,6 +94,7 @@ function displayBooks() {
     }
     const contentRow = document.createElement("div");
     contentRow.className = "contentRow";
+    contentRow.id = `contentRow${i}`;
     const contentOne = document.createElement("div");
     contentOne.className = "contentOne";
     contentOne.innerText = library[i].title;
@@ -75,10 +104,10 @@ function displayBooks() {
     contentTwo.innerText = library[i].author;
     const contentThree = document.createElement("button");
     contentThree.className = "contentThree";
-    contentThree.id = `${i}`;
     contentThree.innerText = library[i].read === true ? "read" : "not read";
-    const contentFour = document.createElement("div");
+    const contentFour = document.createElement("button");
     contentFour.className = "contentFour";
+    contentFour.innerText = "delete";
     contentRow.append(contentOne);
     contentRow.append(contentTwo);
     contentRow.append(contentThree);
@@ -86,8 +115,10 @@ function displayBooks() {
     contentContainer.append(contentRow);
     library[i].add = true;
   }
+  readBtns = document.querySelectorAll(".contentThree");
+  console.log(readBtns);
+  readListener(readBtns);
+  // deleteBtns = document.querySelectorAll(".contentFour");
+  // deleteListener(deleteBtns);
 }
-
-displayBooks();
-
-// Listening for Read Button
+// displayBooks();
